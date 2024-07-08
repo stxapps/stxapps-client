cbb32f64808ec33d67005bc5b57bb7bd946177c8
wit <witwitchayakarn@gmail.com>
2024-07-08 17:53:50 +0700

    Add a link to the blog

--- packages/web/src/components/Landing.js
+++ packages/web/src/components/Landing.js
@@ -129,7 +129,7 @@ const Landing = () => {
             </div>
             <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
               <div className="pt-12 sm:pt-16 lg:pt-20">
-                <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Utilizing Web3 technology from <a className="text-indigo-600 rounded hover:underline focus:outline-none focus:ring-2 focus:ring-indigo-500" href="https://www.hiro.so/stacks-js" target="_blank" rel="noreferrer">Stacks</a></h2>
+                <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Utilizing Web3 technology from <a className="text-indigo-600 rounded hover:underline focus:outline-none focus:ring" href="https://www.hiro.so/stacks-js" target="_blank" rel="noreferrer">Stacks</a></h2>
                 <div className="mt-6 text-gray-500 space-y-6">
                   <p className="text-lg">A technology that allows you to truly own your accounts and your data</p>
                 </div>
@@ -165,7 +165,7 @@ const Landing = () => {
                 <div className="text-left sm:text-center">
                   <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Contact us</h2>
                   <p className="mt-6 mx-auto max-w-2xl text-lg text-gray-500">
-                    <a className="rounded hover:underline focus:outline-none focus:ring-2 focus:ring-gray-500" href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#115;&#116;&#120;&#097;&#112;&#112;&#115;&#046;&#099;&#111;&#109;"><span className="e-mail" data-user="troppus" data-website="moc.sppaxts"></span></a>
+                    <a className="rounded hover:underline focus:outline-none focus:ring" href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#115;&#116;&#120;&#097;&#112;&#112;&#115;&#046;&#099;&#111;&#109;"><span className="e-mail" data-user="troppus" data-website="moc.sppaxts"></span></a>
                   </p>
                 </div>
               </div>
@@ -181,7 +181,15 @@ const Landing = () => {
           </div>
           <div className="mt-8 md:mt-0">
             <div className="flex justify-center sm:justify-end">
-              <a className="text-gray-400 hover:text-gray-500" href="https://github.com/stxapps" target="_blank" rel="noreferrer">
+              <a className="text-gray-400 rounded p-1 hover:text-gray-500 focus:outline-none focus:ring" href="https://medium.com/@stxapps" target="_blank" rel="noreferrer">
+                <span className="sr-only">Medium</span>
+                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 1043.63 592.71" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path d="M588.67,296.36c0,163.67-131.78,296.35-294.33,296.35S0,460,0,296.36,131.78,0,294.34,0,588.67,132.69,588.67,296.36" />
+                  <path d="M911.56,296.36c0,154.06-65.89,279-147.17,279s-147.17-124.94-147.17-279,65.88-279,147.16-279,147.17,124.9,147.17,279" />
+                  <path d="M1043.63,296.36c0,138-23.17,249.94-51.76,249.94s-51.75-111.91-51.75-249.94S963.29,46.42,991.87,46.42s51.76,111.9,51.76,249.94" />
+                </svg>
+              </a>
+              <a className="ml-4 text-gray-400 rounded p-1 hover:text-gray-500 focus:outline-none focus:ring" href="https://github.com/stxapps" target="_blank" rel="noreferrer">
                 <span className="sr-only">GitHub</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
